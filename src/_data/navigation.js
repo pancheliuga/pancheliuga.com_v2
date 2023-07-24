bf1162ed0a792dbe468801ca67216cc5b9185b4f
pancheliuga <pan4ik@gmail.com>
2023-07-24 17:56:00 +0300

    chore: Add base starter - eleventy-excellent

---
+++ src/_data/navigation.js
@@ -0,0 +1,26 @@
+module.exports = {
+  top: [
+    {
+      text: 'About',
+      url: '/about/'
+    },
+    {
+      text: 'Projects',
+      url: '/projects/'
+    },
+    {
+      text: 'Blog',
+      url: '/blog/'
+    }
+  ],
+  bottom: [
+    {
+      text: 'Imprint',
+      url: '/imprint/'
+    },
+    {
+      text: 'Privacy',
+      url: '/privacy/'
+    }
+  ]
+};
